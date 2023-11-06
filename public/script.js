e88fea0622aff77460b849875ccdce28561b113c
Ayush Patel <ayushbp@email.sc.edu>
2023-11-06 14:39:35 -0500

    ass13

--- public/script.js
+++ public/script.js
@@ -1,11 +1,10 @@
 const showTeams = async () => {
     console.log("teams!");
-    
+
     const teamJSON = await getJSON();
-    if (teamJSON == " ") {
+    if (!teamJSON || teamJSON.length === 0) {
         return;
     }
-    
 
     let teamDiv = document.getElementById("team-list");
 
@@ -19,24 +18,36 @@ const showTeams = async () => {
 
         let img = document.createElement("img");
         section.append(img);
-        img.scr = "http://localhost300/" + team.img;
+        img.src = `http://localhost:3000/${team.img}`; // Corrected 'src' attribute
 
+        let pCity = document.createElement("p");
+        section.append(pCity);
+        pCity.innerHTML = `City: ${team.city}`;
 
+        let pChampionships = document.createElement("p");
+        section.append(pChampionships);
+        pChampionships.innerHTML = `Championships: ${team.championships}`;
+
+        let pMascot = document.createElement("p");
+        section.append(pMascot);
+        pMascot.innerHTML = `Mascot: ${team.mascot}`;
+
+        let pColors = document.createElement("p");
+        section.append(pColors);
+        pColors.innerHTML = `Colors: ${team.colors.join(", ")}`;
     });
 };
 
 const getJSON = async () => {
-  try {
+    try {
         let response = await fetch("http://localhost:3000/api/teams");
         return await response.json();
-    } catch(error) {
-        console.log("error json");
-        return "";
+    } catch (error) {
+        console.log("Error fetching JSON:", error);
+        return [];
     }
-
-    return teamJSON;
-}
+};
 
 window.onload = () => {
     showTeams();
-}
+};
